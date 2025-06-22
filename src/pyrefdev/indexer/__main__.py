0191dea770e48c3b49017d3221c23eacb0f9e21f
Yilei Yang <hi@mangoumbrella.com>
2025-06-22 14:29:48 -0700

    Add a simple crawler to the pyrefdev-indexer CLI.

--- src/pyrefdev/indexer/__main__.py
+++ src/pyrefdev/indexer/__main__.py
@@ -1,13 +1,17 @@
 import cyclopts
 
 import pyrefdev
+from pyrefdev.indexer.crawl_docs import crawl_docs
+from pyrefdev.indexer.config import console
 
 
 app = cyclopts.App(
     name="pyrefdev-indexer",
     help="The indexer for pyref.dev.",
     version=pyrefdev.__version__,
+    console=console,
 )
+app.command(crawl_docs)
 
 
 if __name__ == "__main__":
