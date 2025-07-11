74e7f7e39e0115a389d927955634d41c62c217d7
Yilei Yang <hi@mangoumbrella.com>
2025-07-11 11:47:03 +0800

    Indexer: skip non-existent PyPI packages.

--- src/pyrefdev/indexer/crawl_pypi.py
+++ src/pyrefdev/indexer/crawl_pypi.py
@@ -1,4 +1,5 @@
 import json
+from urllib.error import HTTPError
 
 from rich.progress import track
 
@@ -17,4 +18,10 @@ def crawl_pypi(*, refresh: bool = True, index: Index = Index()) -> None:
         rows, description=f"Reading {len(rows)} packages", console=console
     ):
         package_name = row["project"]
-        index.fetch_pypi_data(package_name, refresh=refresh)
+        try:
+            index.fetch_pypi_data(package_name, refresh=refresh)
+        except HTTPError as e:
+            if e.code == 404:
+                console.warning(f"Package {package_name} not found")
+                continue
+            raise
